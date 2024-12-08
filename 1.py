f4b60c25e8f19905f3edb9a888167be70edb8315
31071997 <zferdous35@gmail.com>
2024-12-09 02:40:52 +0600

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  8 14:48:10 2024
+
+@author: ASUS
+"""
+a, b = input().split(' ')
+b = int(b)
+a = int(a)
+if(a == b):
+    print("Square")
+else:
+    print("Not Square")
