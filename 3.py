cc5f4b48112b103ce65ff5ec8ec6827f449783be
31071997 <zferdous35@gmail.com>
2024-12-19 12:37:55 +0600

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 19 12:27:34 2024
+
+@author: ASUS
+"""
+attandence = int(input())
+day = 365
+percantage = (attandence/365)*100
+if percantage >= 75:
+    print("yes")
+else:
+    print("No")
+    
